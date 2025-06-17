27d78a464d244f40a521f546cd60974186f0e7fb
Malcom Otieno Adrian <malcolmotieno16@gmail.com>
2025-06-17 14:22:35 +0300

    add initial implementation of greetStudents function and student array

---
+++ variables.js
@@ -0,0 +1,7 @@
+const students = ['Tracy', 'Gail', 'Mike', 'Shama', 'Trevor'];
+
+function greetStudents(students) {
+    console.log("Hello, " + students[2] + " and " + students[3] + "!");
+ }
+greetStudents(students);
+ anatomy
